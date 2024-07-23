544d0de063f8a3734c71fec10b9ca96e2bc63e58
Banja David Lotigo <innocdavid@gmail.com>
2024-07-23 12:24:55 +0300

    [array manipulation] initial commit

---
+++ arrayManipulation/MainClass.java
@@ -0,0 +1,10 @@
+package arrayManipulation;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("===== main =======");
+        Array numbers = new Array(4);
+        numbers.print();
+    }
+    
+}
